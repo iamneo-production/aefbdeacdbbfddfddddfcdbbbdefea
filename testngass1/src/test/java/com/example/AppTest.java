d732b808c4c4ff7d49513ad05d58e0b146c0cdc7
Iamneo <techadmin@iamneo.ai>
2023-08-11 12:21:28 +0000

    Backup Commit

--- testngass1/src/test/java/com/example/AppTest.java
+++ testngass1/src/test/java/com/example/AppTest.java
@@ -1,48 +1,51 @@
-package com.example;
-
-import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
-public class AppTest {
+public class BrowserAutomationTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        // Set up WebDriver and open Chrome browser
+        System.setProperty("webdriver.chrome.driver", "path_to_chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
 
     @Test
-    public void testSelenium() throws InterruptedException {
-        // Create a new Chrome driver
-        System.setProperty("webdriver.chrome.driver",
-                "C:\\Testing\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        // Maximize the browser window
-        driver.manage().window().maximize();
-
-        // Navigate to the iamneo.ai website
+    public void testIamNeoTitle() {
+        // Navigate to iamneo.ai
         driver.get("http://iamneo.ai");
+        
+        // Verify page title
+        String expectedTitle = "We are Hiring!";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle, "Title does not match");
+    }
 
-        // Write a method to print PASS if the title of the page matches with “We are
-        // Hiring!” else FAIL.
-        boolean isTitleMatch = driver.getTitle().equals("We are Hiring!");
-        if (isTitleMatch) {
-            System.out.println("PASS");
-        } else {
-            System.out.println("FAIL");
-        }
-
-        // Navigate to the Facebook page
+    @Test
+    public void testFacebookNavigation() {
+        // Navigate to Facebook
         driver.get("https://www.facebook.com");
-
-        // Navigate back to the iamneo.ai website
+        
+        // Navigate back to iamneo.ai
         driver.navigate().back();
-
+        
         // Print the URL of the current page
-        System.out.println(driver.getCurrentUrl());
-
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        
         // Navigate forward
         driver.navigate().forward();
-
+        
         // Reload the page
         driver.navigate().refresh();
+    }
 
+    @AfterClass
+    public void tearDown() {
         // Close the browser
         driver.quit();
     }
